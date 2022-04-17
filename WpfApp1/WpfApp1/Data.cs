c3bafa21ca965645ee7ccb6985ea54608b1b04fe
Tanita Mošić <tanitamosic@gmail.com>
2022-04-17 17:23:14 +0200

    XAML kostur sa combobox itemima za fikse vrednosti

---
+++ WpfApp1/WpfApp1/Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Data
+    {
+        public static string Valuta { get; set; }
+        public static string Trziste { get; set; }
+        public static string Vrsta { get; set; }
+        public static string Interval { get; set; }
+        public static string Vreme { get; set; }
+
+
+    }
+}
